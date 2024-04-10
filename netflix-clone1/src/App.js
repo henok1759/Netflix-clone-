af9b9745f686326ef8d49e5c5145a827bb136a98
BirukMetaferia <145568670+BirukMetaferia@users.noreply.github.com>
2024-04-10 20:33:03 +0600

    banner axios and requests

--- netflix-clone1/src/App.js
+++ netflix-clone1/src/App.js
@@ -2,10 +2,12 @@
 import './App.css';
 import Header from "./Components/Header/Header";
 import Footer from './Components/Footer/Footer';
+import Banner from './Components/Banner/Banner';
 function App() {
   return (
     <>
    <Header/>
+   <Banner/>
    <Footer/>
     </>
   );
