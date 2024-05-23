bc755608cf76211795d6dc952bd600e21fbd9469
BiniyamNebso <biniyamnebso@gmail.com>
2024-05-23 14:12:21 +0600

    update

--- src/Components/Rows/RowList/RowList.js
+++ src/Components/Rows/RowList/RowList.js
@@ -17,7 +17,7 @@ const RowList = () => {
 <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
 <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
 <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-<Row title="TV Shows" fetchUrl={requests.fetchTVShow} />
+<Row title="TV Shows" fetchUrl={requests.fetchTVShows} />
 <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </>
   );
